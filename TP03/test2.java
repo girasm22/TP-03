ddff164a2115698b632218bf8e59bae542260a65
girasmatahari <71244688+girasmatahari@users.noreply.github.com>
2020-09-21 14:58:01 +0700

    test2.java

---
+++ TP03/test2.java
@@ -0,0 +1,8 @@
+public class test2{
+   public static void main(String[]args){ 
+      int umur = 18;
+      String name = "Giras";
+      System.out.println("Halo," + name);
+      System.out.println("Ternyata usiamu baru " + umur + " tahun ya");
+   }
+}
